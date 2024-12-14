72808b9b349a8bbd889c6f18bc098e7c44fc9306
Monius <m1105930979@gmail.com>
2024-12-14 20:56:49 +0800

    v0.1.6

--- lib/app.dart
+++ lib/app.dart
@@ -67,8 +67,7 @@ class _ClickAppState extends State<ClickApp> {
         },
         backgroundColor: widget.appTheme.gradientMiddleColor,
         elevation: 10,
-        type: BottomNavigationBarType.fixed,
-        
+        type: BottomNavigationBarType.fixed
       ),
     );
   }
